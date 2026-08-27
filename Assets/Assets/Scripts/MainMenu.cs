fc4b96b3f2c09bfb5d0abcb4b2a46cec32848080
Chatpanu <tvggop@gmail.com>
2026-08-28 01:42:42 +0700

    Menu

---
+++ Assets/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
