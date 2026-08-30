690c2c08f5bc71729cc150d0c2b014c22306c1b7
Chatpanu <tvggop@gmail.com>
2026-08-30 17:03:59 +0700

    add blow sound

---
+++ Assets/Assets/Scripts/PlayerSoundController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayerSoundController : MonoBehaviour
+{
+    [Header("Sound Clips")]
+    [SerializeField] private AudioClip blowSound;
+    private AudioSource audioSource;
+    private bool wasHolding = false;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        bool isHolding = Input.GetMouseButton(0) || Input.GetKey(KeyCode.Space);
+
+        if (isHolding && !wasHolding)
+        {
+            PlayBlowSound();
+        }
+        else if (!isHolding && wasHolding)
+        {
+            StopBlowSound();
+        }
+
+        wasHolding = isHolding;
+    }
+
+    private void PlayBlowSound()
+    {
+        if (blowSound == null) return;
+        audioSource.clip = blowSound;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    private void StopBlowSound()
+    {
+        if (audioSource.isPlaying && audioSource.clip == blowSound)
+        {
+            audioSource.Stop();
+        }
+    }
+}
